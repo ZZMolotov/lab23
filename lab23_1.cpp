fec7cd3a83d6e1c197260d584e1ecba5bd46a879
ZZMolotov <zzmolotov8559@gmail.com>
2026-03-06 23:19:03 +0700

    lab23 fixed

--- lab23_1.cpp
+++ lab23_1.cpp
@@ -20,20 +20,71 @@ string toUpperStr(string x){
     return y;
 }
 
-void importDataFromFile(){
-
+void importDataFromFile(string filename, vector<string> &names, vector<int> &score, vector<char> &grade){
+    ifstream fin (filename);
+    string s;
+    while(getline(fin,s)){
+        const char* cs = s.c_str();
+        char format[] = "%[^:]: %d %d %d";
+        char name[100];
+        int score1, score2, score3;
+        sscanf(cs, format, name, &score1, &score2, &score3);
+        int sum = score1 + score2 + score3;
+        char gradecal = score2grade(sum);
+        string namestring = name;
+        names.push_back(namestring);
+        score.push_back(sum);
+        grade.push_back(gradecal);
+    }
 }
 
-void getCommand(){
-
+void getCommand(string &command, string &key){
+    string temp;
+    cout<<"Please input your command:"<<endl;
+    getline(cin, temp);
+    const char * Ctemp = temp.c_str();
+    char Ccommand[100];
+    char Cname[100];
+    char format[] = "%s %[^\n]";
+    sscanf(Ctemp, format, Ccommand, Cname);
+    command = Ccommand;
+    key = Cname;
 }
 
-void searchName(){
-
+void searchName(vector <string> &name, vector<int> &score, vector<char> &grade, string key){
+    bool found = false;
+    for (unsigned int i = 0; i < name.size(); i++){
+        if(toUpperStr(name[i]) == key){
+            cout<<"---------------------------------" << "\n";
+            cout<<name[i]<< "'s score = "<<score[i]<<"\n";
+            cout<<name[i]<< "'s grade = "<<grade[i]<<"\n";
+            cout<<"---------------------------------" << "\n";
+            found = true;
+        }
+        
+        
+    }
+    if (!found){
+            cout<<"---------------------------------" << "\n";
+            cout<<"Cannot found."<<"\n";
+            cout<<"---------------------------------" << "\n";
+        }  
 }
 
-void searchGrade(){
-
+void searchGrade(vector<string> &name, vector<int> &score, vector<char> &grade, string key){
+    bool found = false;
+    char charkey = key[0];
+    cout<<"---------------------------------" << "\n";
+    for(unsigned int i = 0; i < name.size(); i++){
+        if(grade[i] == charkey){
+            cout<<name[i]<<" ("<<score[i]<<")"<<"\n";
+            found = true;
+        }
+    }
+    if (!found){
+        cout<<"Cannot found."<<"\n";
+    }
+    cout<<"---------------------------------" << "\n";
 }
 
 
